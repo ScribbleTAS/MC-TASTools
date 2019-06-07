3c80356a7984089cb09fc2cef4ed2baec4ba5da2
Scribble <ScribbleLP@users.noreply.github.com>
2019-06-07 21:43:10 +0200

    Diplay the damage dealt to the dragon in the console...

--- src/main/java/de/scribble/lp/TASTools/enderdragon/DragonEvents.java
+++ src/main/java/de/scribble/lp/TASTools/enderdragon/DragonEvents.java
@@ -1,7 +1,10 @@
 package de.scribble.lp.TASTools.enderdragon;
 
 import de.scribble.lp.TASTools.ClientProxy;
+import de.scribble.lp.TASTools.CommonProxy;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.boss.EntityDragon;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
@@ -11,6 +14,12 @@ public class DragonEvents {
 	public void onKeyPress(InputEvent.KeyInputEvent ev) {
 		if(ClientProxy.DragonKey.isPressed()) {
 			Minecraft.getMinecraft().thePlayer.sendChatMessage("/dragon");
+		}
+	}
+	@SubscribeEvent
+	public void displayDamageinConsole(LivingHurtEvent ev) {
+		if (ev.entity instanceof EntityDragon) {
+			CommonProxy.logger.info("DragonDamage: "+ev.ammount);
 		}
 	}
 }
