b15bd9f01d7df95342987dea99f85482f6dae485
Scribble <ScribbleLP@users.noreply.github.com>
2019-07-24 17:41:54 +0200

    Nerfing Dupekey

--- src/main/java/de/scribble/lp/TASTools/duping/DupeEvents.java
+++ src/main/java/de/scribble/lp/TASTools/duping/DupeEvents.java
@@ -6,10 +6,12 @@ import de.scribble.lp.TASTools.ClientProxy;
 import de.scribble.lp.TASTools.CommonProxy;
 import de.scribble.lp.TASTools.ModLoader;
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class DupeEvents {
 	private Minecraft mc= Minecraft.getMinecraft();
@@ -38,8 +40,38 @@ public class DupeEvents {
 	public void pressKeybinding(InputEvent.KeyInputEvent ev){
 		if (dupingenabled&&!FMLCommonHandler.instance().getMinecraftServerInstance().isDedicatedServer()){
 			if(ClientProxy.DupeKey.isPressed()){
+				startStopping();
 				ModLoader.NETWORK.sendToServer(new DupePacket());
 			}
 		}
+	}
+	public void startStopping() {
+		StopMoving stopit = new StopMoving();
+		Minecraft.getMinecraft().player.motionX=0;
+		Minecraft.getMinecraft().player.motionY=0;
+		Minecraft.getMinecraft().player.motionZ=0;
+		Minecraft.getMinecraft().player.velocityChanged=true;
+		MinecraftForge.EVENT_BUS.register(stopit);
+	}
+}
+class StopMoving{
+	Minecraft mc = Minecraft.getMinecraft();
+	int length=0;
+	@SubscribeEvent
+	public void stopMoving(TickEvent ev) {
+		mc.gameSettings.keyBindForward.pressed=false;
+		mc.gameSettings.keyBindBack.pressed=false;
+		mc.gameSettings.keyBindRight.pressed=false;
+		mc.gameSettings.keyBindLeft.pressed=false;
+		mc.gameSettings.keyBindJump.pressed=false;
+		mc.gameSettings.keyBindSprint.pressed=false;
+		mc.gameSettings.keyBindSneak.pressed=false;
+		mc.gameSettings.keyBindDrop.pressed=false;
+		mc.gameSettings.keyBindAttack.pressed=false;
+		mc.gameSettings.keyBindUseItem.pressed=false;
+		if(length>10) {
+			MinecraftForge.EVENT_BUS.unregister(this);
+		}
+		length++;
 	}
 }
