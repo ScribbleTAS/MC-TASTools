2c6a5270c4277073c271c33ded0125f06d0f097e
Scribble <ScribbleLP@users.noreply.github.com>
2019-07-25 18:41:45 +0200

    Buffing dupekey again

--- src/main/java/de/scribble/lp/TASTools/duping/DupeEvents.java
+++ src/main/java/de/scribble/lp/TASTools/duping/DupeEvents.java
@@ -8,16 +8,18 @@ import de.scribble.lp.TASTools.ModLoader;
 import de.scribble.lp.TASTools.savestates.gui.GuiSavestateIngameMenu;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiIngameMenu;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
 
 public class DupeEvents {
 	private Minecraft mc= Minecraft.getMinecraft();
+	protected static EntityPlayer playa;
 	public static boolean dupingenabled;
-	protected static int length=0;
 	
 	@SubscribeEvent
 	public void onCloseServer(PlayerEvent.PlayerLoggedOutEvent ev){
@@ -41,38 +43,49 @@ public class DupeEvents {
 	@SubscribeEvent
 	public void pressKeybinding(InputEvent.KeyInputEvent ev){
 		if(ClientProxy.DupeKey.isPressed()&&dupingenabled){
-			startStopping();
 			ModLoader.NETWORK.sendToServer(new DupePacket());
 		}
 	}
-	public void startStopping() {
+	public void startStopping(EntityPlayer player) {
 		StopMoving stopit = new StopMoving();
+		playa= player;
 		Minecraft.getMinecraft().thePlayer.motionX=0;
 		Minecraft.getMinecraft().thePlayer.motionY=0;
 		Minecraft.getMinecraft().thePlayer.motionZ=0;
 		Minecraft.getMinecraft().thePlayer.velocityChanged=true;
+		playa.setEntityInvulnerable(true);
 		MinecraftForge.EVENT_BUS.register(stopit);
 		length=0;
 	}
 
 }
 class StopMoving extends DupeEvents{
-	Minecraft mc = Minecraft.getMinecraft();
-
+	private Minecraft mc = Minecraft.getMinecraft();
+	private int length=0;
+	
 	@SubscribeEvent
-	public void stopMoving(TickEvent ev) {
-		if (length < 10) {
-			mc.gameSettings.keyBindForward.pressed = false;
-			mc.gameSettings.keyBindBack.pressed = false;
-			mc.gameSettings.keyBindRight.pressed = false;
-			mc.gameSettings.keyBindLeft.pressed = false;
-			mc.gameSettings.keyBindJump.pressed = false;
-			mc.gameSettings.keyBindSprint.pressed = false;
-			mc.gameSettings.keyBindSneak.pressed = false;
-			mc.gameSettings.keyBindDrop.pressed = false;
-			mc.gameSettings.keyBindAttack.pressed = false;
-			mc.gameSettings.keyBindUseItem.pressed = false;
-			length++;
+	public void stopMoving(TickEvent.ClientTickEvent ev) {
+		if(ev.phase==Phase.START){
+			if (length < 5) {
+				mc.gameSettings.keyBindForward.pressed = false;
+				mc.gameSettings.keyBindBack.pressed = false;
+				mc.gameSettings.keyBindRight.pressed = false;
+				mc.gameSettings.keyBindLeft.pressed = false;
+				mc.gameSettings.keyBindJump.pressed = false;
+				mc.gameSettings.keyBindSprint.pressed = false;
+				mc.gameSettings.keyBindSneak.pressed = false;
+				mc.gameSettings.keyBindDrop.pressed = false;
+				mc.gameSettings.keyBindAttack.pressed = false;
+				mc.gameSettings.keyBindUseItem.pressed = false;
+			}
+			if(length==60) {
+				if (!playa.isCreative()&&!playa.isSpectator()) {
+					playa.setEntityInvulnerable(false);
+				}
+			}
+			if (length<=60) {
+				length++;
+			}
 		}
 	}
 }
