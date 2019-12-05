8f46203c03edfaeccc6de6d3afecb6c6d8f5b2cd
Scribble <ScribbleLP@users.noreply.github.com>
2019-12-05 21:39:41 +0100

    Changing my name to something colorful!

--- src/main/java/de/scribble/lp/TASTools/name/NameForcer.java
+++ src/main/java/de/scribble/lp/TASTools/name/NameForcer.java
@@ -20,8 +20,41 @@ public class NameForcer {
 	@SubscribeEvent
 	public void onPlayerCreated(NameFormat ev) {
 		if(ev.getDisplayname().equalsIgnoreCase("TAS_bot"))ev.setDisplayname(TextFormatting.GOLD+"TASBot");
-		else if(ev.getDisplayname().equalsIgnoreCase("ScribbleLP"))ev.setDisplayname(TextFormatting.DARK_AQUA+"Scribble");
+		else if(ev.getDisplayname().equalsIgnoreCase("ScribbleLP"))ev.setDisplayname(stringToRainbow("Scribble", false));
 		else if(ev.getDisplayname().equalsIgnoreCase("Curcuit_Block"))ev.setDisplayname(TextFormatting.BLUE+"Curcuit Block");
 		else ev.setDisplayname(TextFormatting.GRAY+ev.getDisplayname());
+	}
+	
+	
+	
+	public static String stringToRainbow(String parString, boolean parReturnToBlack)
+	{
+	   int stringLength = parString.length();
+	   if (stringLength < 1)
+	   {
+	      return "";
+	   }
+	   String outputString = "";
+	   TextFormatting[] colorChar = 
+	      {
+	         TextFormatting.RED,
+	         TextFormatting.GOLD,
+	         TextFormatting.YELLOW,
+	         TextFormatting.GREEN,
+	         TextFormatting.AQUA,
+	         TextFormatting.BLUE,
+	         TextFormatting.LIGHT_PURPLE,
+	         TextFormatting.DARK_PURPLE
+	      };
+	   for (int i = 0; i < stringLength; i++)
+	   {
+	      outputString = outputString+colorChar[i%8]+parString.substring(i, i+1);
+	   }
+	   // return color to a common one after (most chat is white, but for other GUI might want black)
+	   if (parReturnToBlack)
+	   {
+	      return outputString+TextFormatting.BLACK;
+	   }
+	   return outputString+TextFormatting.WHITE;
 	}
 }
