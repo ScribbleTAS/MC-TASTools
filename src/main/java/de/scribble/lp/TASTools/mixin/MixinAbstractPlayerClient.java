208989d989f56fa97f48ccbe8b71021fef42774a
Scribble <ScribbleLP@users.noreply.github.com>
2020-01-07 15:26:16 +0100

    Deleted NameForcer and added capes
    Also French language yay

--- src/main/java/de/scribble/lp/TASTools/mixin/MixinAbstractPlayerClient.java
+++ src/main/java/de/scribble/lp/TASTools/mixin/MixinAbstractPlayerClient.java
@@ -1,21 +1,28 @@
 package de.scribble.lp.TASTools.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.client.network.NetworkPlayerInfo;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.StringUtils;
 
 @Mixin(AbstractClientPlayer.class)
 public class MixinAbstractPlayerClient {
-	
+	@Shadow
+	NetworkPlayerInfo playerInfo;
 	@Inject(method="getLocationSkin", at=@At("HEAD"), cancellable=true)
 	private void redogetLocationSkin(CallbackInfoReturnable<ResourceLocation> ci) {
 		ci.setReturnValue(new ResourceLocation("tastools:textures/rob.png"));
+		ci.cancel();
+	}
+	@Inject(method="getLocationCape", at=@At("HEAD"), cancellable=true)
+	private void redogetLocationCape(CallbackInfoReturnable<ResourceLocation> ci) {
+		if(playerInfo.getGameProfile().getName().equals("TAS_bot"))ci.setReturnValue(new ResourceLocation("tastools:textures/capes/tascape.png"));
+		else ci.setReturnValue(new ResourceLocation("tastools:textures/capes/bottlecape.png"));
 		ci.cancel();
 	}
 }
