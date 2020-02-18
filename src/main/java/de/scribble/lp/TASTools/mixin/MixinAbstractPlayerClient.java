699ecaa2c9f5abcb50f122b612136d56f2cb73b6
Scribble <ScribbleLP@users.noreply.github.com>
2020-02-18 16:07:16 +0100

    Added stuff to the default background -A potion and a redish tint -Fixed an oopsie with TAS_Bots cape...

--- src/main/java/de/scribble/lp/TASTools/mixin/MixinAbstractPlayerClient.java
+++ src/main/java/de/scribble/lp/TASTools/mixin/MixinAbstractPlayerClient.java
@@ -28,13 +28,13 @@ public class MixinAbstractPlayerClient {
 	}
 	@Inject(method="getLocationCape", at=@At("HEAD"), cancellable=true)
 	private void redogetLocationCape(CallbackInfoReturnable<ResourceLocation> ci) {
-		if(playerInfo.getGameProfile().getId().toString().equals("f3112feb-00c1-4de8-9829-53b940342996"))ci.setReturnValue(scribblecape);
-		else if(playerInfo.getGameProfile().getId().toString().equals("79658727-8778-4dc0-9d02-21f20ed913e7"))ci.setReturnValue(creepercape);
-		else if(playerInfo.getGameProfile().getId().toString().equals("79658727-8778-4dc0-9d02-21f20ed913e7"))ci.setReturnValue(creepercape);
-		else if(playerInfo.getGameProfile().getId().toString().equals("a962e219-c16e-4c3b-b38b-b675d10f92ee"))ci.setReturnValue(wellcape);
-		else if(playerInfo.getGameProfile().getId().toString().equals("e5687c44-65c3-4e76-a233-2550a5597ddc"))ci.setReturnValue(vaughcape);
-		else if(playerInfo.getGameProfile().getId().toString().equals("839474a8-ba49-468b-9246-ed80c78383aa"))ci.setReturnValue(admira23MattyBcape);
-		else ci.setReturnValue(scribblecape);
+		if(playerInfo.getGameProfile().getId().toString().equals("f3112feb-00c1-4de8-9829-53b940342996"))ci.setReturnValue(scribblecape); //ScribbleLP
+		else if(playerInfo.getGameProfile().getId().toString().equals("b8abdafc-5002-40df-ab68-63206ea4c7e8"))ci.setReturnValue(tascape); //TAS_Bot
+		else if(playerInfo.getGameProfile().getId().toString().equals("79658727-8778-4dc0-9d02-21f20ed913e7"))ci.setReturnValue(creepercape); //xdTAG_CLAN
+		else if(playerInfo.getGameProfile().getId().toString().equals("a962e219-c16e-4c3b-b38b-b675d10f92ee"))ci.setReturnValue(wellcape); //mcmaxmcmc
+		else if(playerInfo.getGameProfile().getId().toString().equals("e5687c44-65c3-4e76-a233-2550a5597ddc"))ci.setReturnValue(vaughcape); //vaugh gaming
+		else if(playerInfo.getGameProfile().getId().toString().equals("839474a8-ba49-468b-9246-ed80c78383aa"))ci.setReturnValue(admira23MattyBcape); //Curcuit_Block
+		else ci.setReturnValue(bottlecape); //Everyone else
 		ci.cancel();
 	}
 }
